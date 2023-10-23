bf54185dc18c2af02bc598632a8c9cd556aceadb
olesybabaeva <olesybabaeva@gmail.com>
2023-10-22 17:18:10 -0700

    Добавили решение задачи

--- Sem_02_z_01/Program.cs
+++ Sem_02_z_01/Program.cs
@@ -1,1 +1,15 @@
-﻿
+﻿// Напишите программу, которая принимает на вход 
+// трёхзначное число и на выходе показывает 
+// вторую цифру этого числа.
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+int number = new Random().Next(100,1000);
+
+Console.WriteLine($"Рандомное число: {number}");
+
+int firstDigit = number % 100;
+int secondDigit = firstDigit / 10;
+
+Console.WriteLine($"{number} -> {secondDigit}");
